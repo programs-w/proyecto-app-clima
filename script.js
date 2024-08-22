d61de40b1bc1c73de6879a1a8edff4e7aa9943aa
programs-w <wban_@hotmail.com>
2024-08-22 20:15:38 +0200

    Proyecto version 1

---
+++ script.js
@@ -0,0 +1,83 @@
+let api_key='7644cbb126688deb2f939923700e846b';
+let ciudad ='Londres';
+let difKelvin=273.15;
+let urlBase= 'https://api.openweathermap.org/data/2.5/weather'
+
+/*
+document.querySelector("#botonBusqueda").addEventListener("click", () => {
+
+    const ciudad=document.querySelector("#ciudadEntrada").value
+    if (ciudad){
+        fetchDatosClima(ciudad)
+    }
+})
+*/
+
+// prueba
+document.querySelector("#botonBusqueda").addEventListener("click",funcionPrueba)
+document.querySelector("#ciudadEntrada").addEventListener("keydown",teclado)
+
+function teclado(e){
+    let ciudadTecleada = document.querySelector("#ciudadEntrda").value
+    (e.key==="Enter" )
+}
+
+
+
+function funcionPrueba(){
+    const ciudad=document.querySelector("#ciudadEntrada").value
+    if (ciudad){
+        fetchDatosClima(ciudad)
+    }
+
+}
+//hasta aca
+
+
+
+
+
+
+function fetchDatosClima(ciudad){
+    fetch (`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    .then(data => data.json())
+    .then(data =>mostrarDatosClima(data))
+}
+
+function mostrarDatosClima(data){
+console.log(data)
+const divDatosClima=document.querySelector("#datosClima")
+divDatosClima.innerHTML=""
+
+const ciudadNombre=data.name
+const paisNombre=data.sys.country
+const temperatura=data.main.temp
+const humedad=data.main.humidity
+const descripcion=data.weather[0].description
+const icono=data.weather[0].icon
+
+const ciudadTitulo=document.createElement("h2")
+ciudadTitulo.textContent=`${ciudadNombre}, ${paisNombre}`
+
+const temperaturaInfo=document.createElement("p")
+temperaturaInfo.textContent= `La temperatura es: ${(temperatura-difKelvin).toFixed(1)}ºC`
+
+const humedadInfo=document.createElement("p")
+humedadInfo.textContent= `La humedad es: ${humedad}%`
+
+const iconoInfo=document.createElement("img")
+iconoInfo.src=`https://openweathermap.org/img/wn/${icono}@2x.png`
+
+const descripcionInfo=document.createElement("p")
+descripcionInfo.textContent=`La descripción meteorologica es: ${descripcion}`
+
+divDatosClima.appendChild(ciudadTitulo)
+divDatosClima.appendChild(temperaturaInfo)
+divDatosClima.appendChild(humedadInfo)
+divDatosClima.appendChild(iconoInfo)
+divDatosClima.appendChild(descripcionInfo)
+
+}
+
+
+
